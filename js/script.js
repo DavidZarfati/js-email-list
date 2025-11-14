207809850ca982677b6b97c472e5715cd02b3a6a
DavidZarfati <zarda2001@gmail.com>
2025-11-14 15:02:37 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//codice per generare email  https://flynn.boolean.careers/exercises/api/random/mail
+
+axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+// console.log(axios); debug
+
+
