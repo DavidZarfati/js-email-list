c2cbbe534b5065e26daf4f65991f93702994e84c
DavidZarfati <zarda2001@gmail.com>
2025-11-14 15:10:14 +0100

    feat:generate 10 mail con for e chiamata API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //codice per generare email  https://flynn.boolean.careers/exercises/api/random/mail
-
-axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-// console.log(axios); debug
-
-
+for (i = 0; i < 10; i++) {
+    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        // console.log(axios); debug
+        .then(function (resp) {
+            const mailGenerata = resp.data.response
+            console.log(mailGenerata);
+        })
+}
