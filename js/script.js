0e80670c8f1d74045dfd4750b140d6561b33134b
DavidZarfati <zarda2001@gmail.com>
2025-11-14 15:29:42 +0100

    feat: aggiunto bottone che crea 10 nuove mail che sostituiscono le precedenti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 //codice per generare email  https://flynn.boolean.careers/exercises/api/random/mail
-for (i = 0; i < 10; i++) {
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        // console.log(axios); debug
-        .then(function (resp) {
-            const mailGenerata = resp.data.response
-            console.log(mailGenerata);
-        })
-}
+let listaMailGenerate = []
+let mailGenerate = 0
+let mailGenerata = ""
+
+const bottoneMail = document.getElementById("genera-mail")
+// console.log(bottoneMail); debug
+
+bottoneMail.addEventListener("click", function () {
+    for (i = 0; i < 10; i++) {
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            // console.log(axios); debug
+            .then(function (resp) {
+                mailGenerata = resp.data.response
+                listaMailGenerate.push(resp.data.response)
+                console.log(mailGenerata);
+                mailGenerate++
+                if (mailGenerate === 10) {
+                    console.log(listaMailGenerate.join(","))
+                    console.log(listaMailGenerate);
+                    // Li ho mostrati in entrambi i modi (array inteso come lista e lista intesa come lista con virgole) chiedi a samuel,loris,olga
+                    mailGenerate = 0
+                    listaMailGenerate = []
+                }
+            }
+            )
+
+    }
+
+
+})
+
+
