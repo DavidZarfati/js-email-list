951d570bb73bae4aa6e82c265a8389c4f6f65bb1
DavidZarfati <zarda2001@gmail.com>
2025-11-14 17:04:14 +0100

    feat:correzione codice dopo tutor con Loris

--- js/script.js
+++ js/script.js
@@ -1,47 +1,31 @@
 //codice per generare email  https://flynn.boolean.careers/exercises/api/random/mail
-let listaMailGenerate = []
 let mailGenerate = 0
 let mailGenerata = ""
 
-const bottoneMail = document.getElementById("genera-mail")
+const bottoneMail = document.querySelector("#genera-mail")
 // console.log(bottoneMail); debug
-const listaMail = document.getElementById("list-mail")
+const listaMail = document.querySelector("#list-mail")
 // console.log(listaMail); debug
 
 let sommaLi = []
 bottoneMail.addEventListener("click", function () {
     listaMail.innerHTML = '';
+    let htmlList = '';
     for (i = 0; i < 10; i++) {
         axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-            // console.log(axios); debug
             .then(function (resp) {
-                mailGenerata = resp.data.response
-                listaMailGenerate.push(resp.data.response)
-                // console.log(mailGenerata);
-                // let mailGenerataUl = `<li>${mailGenerata}<li>`
-                // listaMail.appendChild(mailGenerataUl)
-                let li = document.createElement('li');
-                li.textContent = mailGenerata;
-                sommaLi.push(li)
-
+                htmlList += `<li>${resp.data.response}</li>`
+                console.log(htmlList);
                 mailGenerate++
                 if (mailGenerate === 10) {
-                    console.log(listaMailGenerate.join(","))
-                    console.log(listaMailGenerate);
-                    sommaLi.forEach(function (liSingolo) {
-                        listaMail.appendChild(liSingolo);
-                    })
-                    // Li ho mostrati in entrambi i modi (array inteso come lista e lista intesa come lista con virgole) chiedi a samuel,loris,olga
-                    mailGenerate = 0
-                    listaMailGenerate = []
-                    sommaLi = []
+                    listaMail.innerHTML = htmlList;
                 }
-            }
-            )
-
+            })
     }
+});
 
 
-})
 
-
+// const li = document.createElement('li');
+// li.textContent = mailGenerata;
+// sommaLi.push(li)
